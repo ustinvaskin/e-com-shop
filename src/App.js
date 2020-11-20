ceffd194e122ed3f8f72c6e41de6435bb623b733
Ustin.Vaskin <51622314+ustinvaskin@users.noreply.github.com>
2020-11-20 15:45:35 +0300

    directory component update to be a functional compoennt

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx'
 
 function App() {
   return (
     <div>
+      {/* always rendered and present */}
+      <Header/> 
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
