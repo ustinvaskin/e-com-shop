1f03a39ecc8d5402c79c1547fe9f409cd15e49af
Ustin.Vaskin <51622314+ustinvaskin@users.noreply.github.com>
2020-11-25 14:05:27 +0300

    Storing user data in our app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,60 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
-import SignInAnSignUp from './pages/sing-in-and-sign-up/sing-in-and-sign-up.component'
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAnSignUp from "./pages/sing-in-and-sign-up/sing-in-and-sign-up.component";
 
-import {auth, createUserProfileDoc} from './firebase/firebase.utils'
+import { auth, createUserProfileDoc } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        // if a user signs in
+        const userRef = await createUserProfileDoc(userAuth); // getting back a user ref
 
-componentDidMount(){
-  this.unsubscribeFromAuth =  auth.onAuthStateChanged(user=> {
-createUserProfileDoc(user)
-  })
-}
+        userRef.onSnapshot((onSnapshot) => {
+          this.setState({
+            currentUser: {
+              id: onSnapshot.id, // getting user id from our database
+              ...onSnapshot.data(), // getting user data from our database
+            },
+          });
+        });
+        this.setState({ currentUser: userAuth }); // setting state for our currentUser
+      }
+    });
+  }
 
-componentWillUnmount(){
-  this.unsubscribeFromAuth()
-}
-
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
   render() {
-  return (
-    <div>
-      {/* always rendered and present */}
-      <Header currentUser = {this.state.currentUser}/> 
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/sign' component={SignInAnSignUp} />
-      </Switch>
-    </div>
-  );
-}
+    return (
+      <div>
+        {/* always rendered and present */}
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/sign" component={SignInAnSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
